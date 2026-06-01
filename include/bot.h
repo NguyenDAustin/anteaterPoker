8a99ff79510e14d4737a43987d15d728e78cd332
aman <amacwan17@gmail.com>
2026-05-31 20:21:11 -0700

    bot initial

--- include/bot.h
+++ include/bot.h
@@ -7,9 +7,14 @@
 
 void initBot(Player_Info *bot, const char *name, int seat, int chips);
 int evalPreFlop(Card hand[2]);
-PlayerAction getBotPreFlopAction(Player_Info *bot, GameState *game);
-PlayerAction getBotPostFlopAction(Player_Info *bot, GameState *game);
-PlayerAction getBotAction(Player_Info *bot, GameState *game);
+int evalPostFlop(GameState *game, int playerIndex);
+int calculateDrawBonus(GameState *game, int playerIndex);
+int calculateCallCost(GameState *game, int playerIndex);
+double calculateCallPrice(GameState *game, int playerIndex);
+int calculateBotRaiseAmount(GameState *game, int playerIndex);
+PlayerAction getBotPreFlopAction(GameState *game, int playerIndex);
+PlayerAction getBotPostFlopAction(GameState *game, int playerIndex);
+PlayerAction getBotAction(GameState *game, int playerIndex);
 
 
 
--- src/bot.c
+++ src/bot.c
@@ -1,4 +1,5 @@
 #include "bot.h"
+#include "hand.h"
 #include <stdlib.h>
 
 void initBot(Player_Info *bot, const char *name, int seat, int chips)
@@ -83,14 +84,172 @@ int evalPreFlop(Card hand[2])
     return score;
 }
 
-PlayerAction getBotPreFlopAction(Player_Info *bot, GameState *game)
+int calculateDrawBonus(GameState *game, int playerIndex)
 {
+    if (game == NULL || playerIndex < 0 || playerIndex >= game->numPlayers)
+        return 0;
+
+    Player_Info *bot = game->players[playerIndex];
+
+    if (bot == NULL)
+        return 0;
+
+    Card cards[TOTAL_HAND_CARDS];
+    int count = 0;
+    int bonus = 0;
+
+    cards[count++] = bot->playerCards[0];
+    cards[count++] = bot->playerCards[1];
+
+    for (int i = 0; i < game->board.count && i < BOARD_SIZE; i++)
+        cards[count++] = game->board.cards[i];
+
+    for (Suit suit = HEARTS; suit <= SPADES; suit++)
+    {
+        if (count_suit(cards, count, suit) >= 4)
+        {
+            bonus += 1;
+            break;
+        }
+    }
+
+    int hasRank[ACE + 1] = {0};
+
+    for (int i = 0; i < count; i++)
+    {
+        if (cards[i].type == NORMAL_CARD && cards[i].rank >= TWO && cards[i].rank <= ACE)
+            hasRank[cards[i].rank] = 1;
+    }
+
+    if (hasRank[ACE])
+        hasRank[1] = 1;
+
+    for (int start = 1; start <= TEN; start++)
+    {
+        int ranksInWindow = 0;
+
+        for (int rank = start; rank < start + 5; rank++)
+            ranksInWindow += hasRank[rank];
+
+        if (ranksInWindow >= 4)
+        {
+            bonus += 1;
+            break;
+        }
+    }
+
+    return bonus;
+}
+
+int evalPostFlop(GameState *game, int playerIndex)
+{
+    if (game == NULL || playerIndex < 0 || playerIndex >= game->numPlayers)
+        return 0;
+
+    Player_Info *bot = game->players[playerIndex];
+
+    if (bot == NULL)
+        return 0;
+
+    Card cards[TOTAL_HAND_CARDS];
+    int count = 0;
+
+    cards[count++] = bot->playerCards[0];
+    cards[count++] = bot->playerCards[1];
+
+    for (int i = 0; i < game->board.count && i < BOARD_SIZE; i++)
+        cards[count++] = game->board.cards[i];
+
+    sort(cards, count);
+
+    int strength = 0;
+
+    if (royalFlush(cards, count))
+        strength = 9;
+    else if (straightFlush(cards, count))
+        strength = 8;
+    else if (four(cards, count))
+        strength = 7;
+    else if (fullHouse(cards, count))
+        strength = 6;
+    else if (flush(cards, count))
+        strength = 5;
+    else if (straight(cards, count))
+        strength = 4;
+    else if (three(cards, count))
+        strength = 3;
+    else if (twoPair(cards, count))
+        strength = 2;
+    else if (pairs(cards, count))
+        strength = 1;
+
+    return strength + calculateDrawBonus(game, playerIndex);
+}
+
+int calculateBotRaiseAmount(GameState *game, int playerIndex)
+{
+    if (game == NULL || playerIndex < 0 || playerIndex >= game->numPlayers)
+        return 0;
+
+    Player_Info *bot = game->players[playerIndex];
+
+    if (bot == NULL || bot->chips <= 0)
+        return 0;
+
+    int raiseAmount = game->pot / 2;
+
+    if (raiseAmount < game->bigBlind)
+        raiseAmount = game->bigBlind;
+
+    if (raiseAmount > bot->chips)
+        raiseAmount = bot->chips;
+
+    return raiseAmount;
+}
+
+int calculateCallCost(GameState *game, int playerIndex)
+{
+    if (game == NULL || playerIndex < 0 || playerIndex >= game->numPlayers)
+        return 0;
+
+    Player_Info *bot = game->players[playerIndex];
+
+    if (bot == NULL)
+        return 0;
+
+    int callCost = game->currentBet - bot->currentBet;
+
+    if (callCost < 0)
+        return 0;
+
+    if (callCost > bot->chips)
+        return bot->chips;
+
+    return callCost;
+}
+
+double calculateCallPrice(GameState *game, int playerIndex)
+{
+    if (game == NULL)
+        return 0.0;
+
+    int callCost = calculateCallCost(game, playerIndex);
+
+    if (callCost <= 0)
+        return 0.0;
+
+    return (double)callCost / (game->pot + callCost);
+}
+
+PlayerAction getBotPreFlopAction(GameState *game, int playerIndex)
+{
+    Player_Info *bot = game->players[playerIndex];
     int score = evalPreFlop(bot->playerCards);
     int callAmount = game->currentBet - bot->currentBet;
 
     if (callAmount <= 0)
     {
-        if (score >= 9)
+        if (score >= 8)
             return (PlayerAction) {RAISE, 0.05 * bot->chips};
         else
             return (PlayerAction) {CHECK, 0};
@@ -114,16 +273,39 @@ PlayerAction getBotPreFlopAction(Player_Info *bot, GameState *game)
     }
 }
 
-PlayerAction getBotPostFlopAction(Player_Info *bot, GameState *game)
+PlayerAction getBotPostFlopAction(GameState *game, int playerIndex)
 {
-    return (PlayerAction){CHECK, 0};
+    int strength = evalPostFlop(game, playerIndex);
+    int callCost = calculateCallCost(game, playerIndex);
+
+    if (callCost == 0) {
+        if (strength >= 4) {
+            return (PlayerAction){RAISE, calculateBotRaiseAmount(game, playerIndex)};
+        }
+
+        return (PlayerAction){CHECK, 0};
+    }
+
+    if (strength >= 5) {
+        return (PlayerAction){RAISE, calculateBotRaiseAmount(game, playerIndex)};
+    }
+
+    if (strength >= 2) {
+        return (PlayerAction){CALL, callCost};
+    }
+
+    if (strength >= 1 && callCost <= game->bigBlind) {
+        return (PlayerAction){CALL, callCost};
+    }
+
+    return (PlayerAction){FOLD, 0};
 }
 
-PlayerAction getBotAction(Player_Info *bot, GameState *game)
+PlayerAction getBotAction(GameState *game, int playerIndex)
 {
     if (game->round == ROUND_PRE_FLOP)
-        return getBotPreFlopAction(bot, game);
+        return getBotPreFlopAction(game, playerIndex);
 
     else
-        return getBotPostFlopAction(bot, game);
+        return getBotPostFlopAction(game, playerIndex);
 }
